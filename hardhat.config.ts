d9aa5c7e883af7ecc8a9049b00f71218d6bf3ce7
Francisco <franciscobortolin2@gmail.com>
2021-12-02 00:32:54 -0300

    Commit final

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -12,12 +12,19 @@ const config: HardhatUserConfig = {
     compilers: [{ version: "0.7.3", settings: {} }],
   },
   networks: {
+    
     hardhat: {},
     localhost: {},
     coverage: {
       url: "http://127.0.0.1:8555", // Coverage launches its own ganache-cli client
     },
+    rinkeby: {
+      url: "https://eth-rinkeby.alchemyapi.io/v2/D1-jom7y3bnZHztWqToO99OH5nhZXspN",
+      accounts: [`0x${"201e8bb83ecd1850c567be053f025e04e0e6928e4fe4c50288b7ce41c0242c3e"}`,],
+      gasPrice: 8000000000
+      }
   }
+  
 };
 
 export default config
